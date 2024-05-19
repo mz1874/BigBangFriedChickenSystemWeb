51a8c1a40a950583ea7b2326d6395e5f85065232
unknown <leeenqi0407@gmail.com>
2024-05-19 12:47:37 +0800

    menu

--- js/menu.js
+++ js/menu.js
@@ -27,8 +27,8 @@ $(document).ready(function() {
                                         <div class="d-flex justify-content-between align-items-center">
                                             <div class="btn-group">
                                                 <text>RM ${item.price.toFixed(2)}</text>
-                                            </div>
-                                            <a href="menu_detail.html?id=${item.id}">
+                                            </div>  
+                                            <a href="menu_detail.html " >
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#7F3720" class="bi bi-plus-square-fill" viewBox="0 0 16 16">
                                                     <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0"/>
                                                 </svg>
