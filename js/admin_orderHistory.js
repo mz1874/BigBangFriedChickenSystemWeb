7b95b07a122739819cd26a6f50bb9702a3b07342
mz1874 <mz1874@aliyun.com>
2024-06-05 00:19:05 +0800

    order history pagination finished

--- js/admin_orderHistory.js
+++ js/admin_orderHistory.js
@@ -1,18 +1,51 @@
 $(document).ready(function () {
 
+    $('#pagination').on('click', '.page-link', function(event) {
+        event.preventDefault(); // 阻止默认行为
+        var page = $(this).text(); // 获取点击的页码
+        selectAllOrders(page)
+    });
 
-    function selectAllOrders(){
+    // 处理 Next 按钮点击事件
+    $('#pagination').on('click', '.page-next', function(event) {
+        event.preventDefault(); // 阻止默认行为
+        var currentPage = parseInt($('#pagination .active a').text()); // 获取当前页码
+        var nextPage = currentPage + 1; // 计算下一页页码
+        selectAllOrders(nextPage); // 加载下一页数据
+    });
+
+    function updatePagination(message) {
+        var pagination = $('#pagination');
+        pagination.empty(); // 清空现有分页导航
+        if (message.has_prev) {
+            pagination.append('<li class="page-item"><a class="page-link" href="#">Previous</a></li>');
+        } else {
+            pagination.append('<li class="page-item disabled"><a class="page-link" href="#">Previous</a></li>');
+        }
+
+        for (var i = 1; i <= message.pages; i++) {
+            if (i === message.page) {
+                pagination.append('<li class="page-item active"><a class="page-link" href="#">' + i + '</a></li>');
+            } else {
+                pagination.append('<li class="page-item"><a class="page-link" href="#">' + i + '</a></li>');
+            }
+        }
+
+        if (message.has_next) {
+            pagination.append('<li class="page-item"><a class="page-link page-next" href="#">Next</a></li>');
+        } else {
+            pagination.append('<li class="page-item disabled"><a class="page-link" href="#">Next</a></li>');
+        }
+    }
+
+    function selectAllOrders(page){
         var dataTosend = {
-            "page":1,
+            "page":page,
             "count":20,
         }
 
-        // 假设有一个处理按钮点击的函数
-        function handleButtonClick(orderId) {
-            alert(`点击了订单ID为 ${orderId} 的按钮`);
-            // 在这里可以执行与该订单ID相关的操作
-        }
         AjaxHelper.sendGet("http://localhost:5000/order/page", dataTosend).then(success=>{
+            updatePagination(success.message)
             let tbody = $('.table-hover tbody');
             tbody.empty(); // 清空现有内容，以免重复添加
             success.message.items.forEach(order => {
@@ -61,7 +94,7 @@ $(document).ready(function () {
 
 
     }
-    selectAllOrders();
+    selectAllOrders(1);
     $('body').on("click", "#operation", function() {
         var orderId = $(this).data("orderid");
         var dataTosend = {
